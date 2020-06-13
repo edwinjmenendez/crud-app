f51bfe65a85a6e188cad0747b10824638a9cd3d7
Edwin <edwinjmenendez@gmail.com>
2020-06-12 22:00:18 -0700

    served static files and rendered html file onto the server browser

--- server/server.js
+++ server/server.js
@@ -6,6 +6,24 @@ const PORT = 3000; // have a port to be listening on
 const reminders = require('./controllers/reminderController'); // after setting up the controller, add routes to each middleware
 app.use(express.json()); // convert everything to json
 
+
+// serve static files to display on browser; index.js and styles.css
+
+// way 1
+// app.get('/assets/index.js', (req, res) => {
+//   return res.sendFile(path.resolve(__dirname, '../client/assets/index.js'))
+// })
+// app.get('/assets/styles.css', (req, res) => {
+//   return res.sendFile(path.join(__dirname, '../client/assets/styles.css'))
+// })
+// way 2
+app.use('/assets', express.static(path.join(__dirname, '../client/assets'))); // serves index.js and css
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, '../client/index.html')) // renders html file
+})
+
+
 // add potential routes to our server
 app.get('/reminders', reminders.getReminder, (req, res) => {
   return res.status(200).json(res.locals.reminders);
