88c2d9903219bf2c01d1fd16420b9015da2f5814
Edwin <edwinjmenendez@gmail.com>
2020-06-12 21:10:02 -0700

    addReminder function works and is added onto database

--- server/controllers/reminderController.js
+++ server/controllers/reminderController.js
@@ -3,7 +3,19 @@ const db = require('../models/db');
 const reminderController = {};
 
 reminderController.addReminder = (req, res, next) => { // cant display each add on postman for some reason
-
+  const { text } = req.body;
+  const params = [ text ]; // has to be in an array to be accepted in query string
+  const queryStr = `INSERT INTO reminders (text) VALUES ($1) RETURNING *;`;
+  db.query(queryStr, params)
+  .then((reminderData) => {
+    console.log(reminderData)
+    res.locals.reminders = reminderData.rows[0];
+    return next();
+  })
+  .catch((err) => {
+    console.log(err)
+    return next(err);
+  })
 }
 
 reminderController.getReminder = (req, res, next) => {
